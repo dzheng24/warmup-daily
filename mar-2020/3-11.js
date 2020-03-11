3486069df5f0eeed437203ae4ba0a777d2d05af6
David Zheng <zhengdav@msu.edu>
2020-03-11 07:33:47 -0700

    added warmup, 3/11

---
+++ mar-2020/3-11.js
@@ -0,0 +1,90 @@
+// 3/11/20 Wednesday
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function forLoop(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function whileLoop(arr) {
+  while (arr.length) {
+    console.log(arr.shift());
+  }
+}
+
+function map(arr, callback) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]));
+  }
+  return newArr;
+}
+const plusTwo = map(numbers, (number => number + 2));
+
+function filter(arr, callback) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+const evensOnly = filter(numbers, (number => number % 2 === 0));
+
+function reduce(arr, callback, result) {
+  for (let i = 0; i < arr.length; i++) {
+    result = callback(result, arr[i], i, arr);
+  }
+  return result;
+}
+const average = reduce(numbers, (totalValue, currentValue, index, arr) => {
+  totalValue += currentValue;
+  if (index === arr.length - 1) {
+    return totalValue / arr.length
+  }
+  return totalValue;
+}, 0);
+
+//-----------------------
+const people = ['Kookla','Fran','Ollie'];
+
+const stuff = {
+  tv: 'huge',
+  radio: 'old',
+  toothbrush: 'frayed',
+  cars: ['Toyota','Mazda']
+}
+
+let state = {};
+
+let newPeople = [];
+let newStuff = {};
+let newState = {};
+
+// Using spread and destructuring assignment, create a new array called newPeople, which is a copy of the people array, with a person named ‘Odie’ added to the beginning and one named ‘Garfield’ added to the end.
+
+newPeople = ['Odie', ...people, 'Garfield'];
+
+// Using spread and destructuring assignment, create a new object called newStuff, which is a copy of the stuff object, with a new car added to the end of the cars array within it
+
+newStuff = {...stuff, cars: [...stuff.cars, 'Rover']};
+
+// Create a state object with keys of people and stuff that contain the people and stuff data.
+// Do this using object destructuring assignment
+
+state = {people, stuff};
+
+
+// Using spread and destructuring assignments, create a new object called newState, repeating the newPeople and newStuff steps above but directly within the people and stuff nodes of the state object (don’t just spread in newPeople and newStuff)
+
+newState = {...state, people: ['Odie', ...people, 'Garfield'], stuff: {...stuff, cars: [...stuff.cars, 'Rover']}};
+console.log(newState);
+
+
+// Prove that the original people, stuff, and state are unchanged.
+console.log(people);
+console.log(stuff);
+console.log(state);
