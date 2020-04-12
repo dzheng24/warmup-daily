89097b249e97bec5db5591ab649883e186c8b62d
David Zheng <zhengdav@msu.edu>
2020-04-12 09:38:34 -0700

    added template for elementary sorts

---
+++ template-sorts.js
@@ -0,0 +1,26 @@
+
+
+// elementary sorts warmup
+
+const numbers = [-44, 0, 55, 26, 27, 13, 19, 2, 24, 9000];
+
+function bubbleSort(arr) {
+
+}
+
+// bubbleSort(numbers);
+// console.log(numbers);
+
+function selectionSort(arr) {
+
+}
+
+// selectionSort(numbers);
+// console.log(numbers);
+
+function insertionSort(arr) {
+
+}
+
+// insertionSort(numbers);
+// console.log(numbers);
